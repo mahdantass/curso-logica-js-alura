31598880283db4bcaf8a81d772ad3bac522d265c
Marcella Dantas <mahsouza8@icloud.com>
2025-03-10 22:35:12 -0300

    app.js

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-// let numeroSecreto = parseInt(Math.random() * 11)
-// let tentativas = 1
-// let chute
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-// alert('Boas vindas ao jogo do número secreto')
+//enquanto -> while
+while(chute != numeroSecreto){
+    chute = prompt(`Escolha um numero entre 1 e ${numeroMaximo}`);
+//se o chute for igual ao numero secreto 
+if(chute == numeroSecreto){
+    //PARA
+    break;
+} else{
+    if(chute > numeroSecreto){
+        alert(`O numero secreto é menor que ${chute}`);
+    }
+    else{
+        alert(`O numero secreto é maior que ${chute}`);
+    }
+    //tentativas = tentivas + 1
+    tentativas++;
+    }
+}
 
-// while (chute != numeroSecreto) {
-//     let chute = prompt('Escolha um número entre 1 e 10')
-//     if (chute == numeroSecreto) {
-//         break
-//     } else {
-//         if (chute > numeroSecreto) {
-//             alert('O número secreto é menor')
-//         } else {
-//             alert('O número secreto é maior')
-//         }
-//     }
-//     tentativas++
-// }
-
-// let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' 
-// alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} ${palavraTentativa}`)
+let palavraTentativa = tentativas > 1 ? 'tentativas': 'tentativa';
+alert(`Isso ai! Voce descobriu o numero secreto: ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
